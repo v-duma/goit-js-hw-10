01ed0962cdd52d1801fdfffc05044e1c17b19d78
v-duma <139060657+v-duma@users.noreply.github.com>
2023-12-05 14:40:56 +0200

    update of selector and loader

--- src/index.js
+++ src/index.js
@@ -9,22 +9,27 @@ const divCatDescription = document.querySelector('.cat-description');
 const loader = document.querySelector('.loader');
 const error = document.querySelector('.error');
 
-loader.classList.replace('loader', 'is-hidden');
+loader.classList.replace('is-hidden', 'loader');
 error.classList.add('is-hidden');
 divCatDescription.classList.add('is-hidden');
+selector.classList.add('is-hidden');
+updateSelect();
 
-let arrBreedsId = [];
-fetchBreeds()
-  .then(data => {
-    data.forEach(element => {
-      arrBreedsId.push({ text: element.name, value: element.id });
-    });
-    new SlimSelect({
-      select: selector,
-      data: arrBreedsId,
-    });
-  })
-  .catch(onFetchError);
+function updateSelect(data) {
+  fetchBreeds(data)
+    .then(data => {
+      loader.classList.replace('loader', 'is-hidden');
+selector.classList.remove('is-hidden'); 
+      let markSelect = data.map(({ name, id }) => {
+        return `<option value ='${id}'>${name}</option>`;
+      });
+      selector.insertAdjacentHTML('beforeend', markSelect);
+      new SlimSelect({
+        select: selector,
+      });
+    })
+    .catch(onFetchError);
+}
 
 selector.addEventListener('change', onSelectBreed);
 
@@ -40,15 +45,17 @@ function onSelectBreed(event) {
   selector.classList.add('is-hidden');
   divCatDescription.classList.add('is-hidden');
 
-  const breedId = event.currentTarget.value;
-  fetchCatByBreed(breedId)
-    .then(data => {
-      loader.classList.replace('loader', 'is-hidden');
-      selector.classList.remove('is-hidden');
-      const { url, breeds } = data[0];
+  const breedId = event.target.value;
 
-      divCatDescription.innerHTML = `<div class="box-img"><img src="${url}" alt="${breeds[0].name}" width="500px"/></div><div class="box"><h1>${breeds[0].name}</h1><p>${breeds[0].description}</p><p><b>Temperament:</b> ${breeds[0].temperament}</p></div>`;
-      divCatDescription.classList.remove('is-hidden');
-    })
-    .catch(onFetchError);
-}
+
+    fetchCatByBreed(breedId)
+      .then(data => {
+        loader.classList.replace('loader', 'is-hidden');
+        selector.classList.remove('is-hidden');
+        const { url, breeds } = data[0];
+
+        divCatDescription.innerHTML = `<div class="box-img"><img src="${url}" alt="${breeds[0].name}" width="500px"/></div><div class="box"><h1>${breeds[0].name}</h1><p>${breeds[0].description}</p><p><b>Temperament:</b> ${breeds[0].temperament}</p></div>`;
+        divCatDescription.classList.remove('is-hidden');
+      })
+      .catch(onFetchError);
+  }
